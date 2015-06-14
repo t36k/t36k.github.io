159b76e9861fa2c088725ebc63def3239df5db5f
t36k <teck.kuoch@gmail.com>
2015-06-13 22:58:46 -0700

    Add masonry. Display thumbs.

--- app.js
+++ app.js
@@ -3,12 +3,74 @@
 
 	app.controller('GalleryController', function() {
 		this.artwork = artworks;
-		this.test = 'test';
 	});
 
 	var artworks = [
 		{
-			name: 'Lazy Cat'
+			name: 'Lazy Cat',
+			thumb: 'images/thumbs/lazy_cat_thumb.jpg',
+			full: 'images/full/lazy_cat.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'The Reader',
+			thumb: 'images/thumbs/reader_thumb.jpg',
+			full: 'images/full/reader.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'Flicker & Billow',
+			thumb: 'images/thumbs/fb_thumb.jpg',
+			full: 'images/full/fb.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'Gatsby',
+			thumb: 'images/thumbs/gatsby_thumb.jpg',
+			full: 'images/full/gatsby.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'Lysol',
+			thumb: 'images/thumbs/lysol_thumb.jpg',
+			full: 'images/full/lysol.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'Shantou',
+			thumb: 'images/thumbs/shantou_thumb.jpg',
+			full: 'images/full/shantou.jpg',
+			layout: 'landscape'
+		},
+		{
+			name: 'Council',
+			thumb: 'images/thumbs/council_thumb.jpg',
+			full: 'images/full/council.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'Dagger',
+			thumb: 'images/thumbs/dagger_thumb.jpg',
+			full: 'images/full/dagger.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'Inhabitants',
+			thumb: 'images/thumbs/inhabitants_thumb.jpg',
+			full: 'images/full/inhabitants.jpg',
+			layout: 'portrait'
+		},
+		{
+			name: 'Wrought',
+			thumb: 'images/thumbs/wrought_thumb.jpg',
+			full: 'images/full/wrought.jpg',
+			layout: 'landscape'
+		},
+		{
+			name: 'Realm',
+			thumb: 'images/thumbs/realm_thumb.jpg',
+			full: 'images/full/realm.jpg',
+			layout: 'portrait'
 		}
 	];
 })();
---
+++ test.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	// init masonry
+	var $grid = $('.grid').masonry({
+		// set itemSelector so .grid-sizer is not used in layout
+		itemSelector: '.grid-item',
+		// use element for option
+		columnWidth: '.grid-sizer',
+		percentPosition: true
+	});
+
+
+	// layout Masonry after each image loads
+	// $grid.imagesLoaded().progress( function() {
+	// 	$grid.masonry('layout');
+	// });
+});
