2fb6b9ec7279e4ddc87fddd88cca3e3c3537beb2
Kuoch, Teck (tk2187) <tk2187@att.com>
2016-01-07 14:09:42 -0800

    Re-instate angular for gallery. Fixed some conflicting styles betweeen bootstrap and packery.

--- app.js
+++ app.js
@@ -9,70 +9,71 @@
 
 	var artworks = [
 		{
-			name: 'Lazy Cat',
-			thumb: 'images/thumbs/lazy_cat_thumb.jpg',
-			full: 'images/full/lazy_cat.jpg',
-			layout: 'portrait'
+			name: 'Council',
+			thumb: 'images/thumbs/council.jpg',
+			full: 'images/full/council.jpg',
+			thumbStyle: ''
 		},
 		{
 			name: 'The Reader',
-			thumb: 'images/thumbs/reader_thumb.jpg',
+			thumb: 'images/thumbs/reader.jpg',
 			full: 'images/full/reader.jpg',
-			layout: 'portrait'
+			thumbStyle: 'height4'
+		},
+		{
+			name: 'Lazy Cat',
+			thumb: 'images/thumbs/lazy_cat.jpg',
+			full: 'images/full/lazy_cat.jpg',
+			thumbStyle: 'width2 height2'
+		},		
+		{
+			name: 'Dagger',
+			thumb: 'images/thumbs/dagger.jpg',
+			full: 'images/full/dagger.jpg',
+			thumbStyle: ''
 		},
 		{
 			name: 'Flicker & Billow',
-			thumb: 'images/thumbs/fb_thumb.jpg',
+			thumb: 'images/thumbs/fb.jpg',
 			full: 'images/full/fb.jpg',
-			layout: 'portrait'
-		},
-		{
-			name: 'Gatsby',
-			thumb: 'images/thumbs/gatsby_thumb.jpg',
-			full: 'images/full/gatsby.jpg',
-			layout: 'portrait'
-		},
-		{
-			name: 'Lysol',
-			thumb: 'images/thumbs/lysol_thumb.jpg',
-			full: 'images/full/lysol.jpg',
-			layout: 'portrait'
+			thumbStyle: 'width2'
 		},
 		{
 			name: 'Shantou',
-			thumb: 'images/thumbs/shantou_thumb.jpg',
+			thumb: 'images/thumbs/shantou.jpg',
 			full: 'images/full/shantou.jpg',
-			layout: 'landscape'
-		},		
-		{
-			name: 'Council',
-			thumb: 'images/thumbs/council_thumb.jpg',
-			full: 'images/full/council.jpg',
-			layout: 'portrait'
+			thumbStyle: 'width4'
 		},
 		{
-			name: 'Dagger',
-			thumb: 'images/thumbs/dagger_thumb.jpg',
-			full: 'images/full/dagger.jpg',
-			layout: 'portrait'
-		},
-		{
-			name: 'Inhabitants',
-			thumb: 'images/thumbs/inhabitants_thumb.jpg',
-			full: 'images/full/inhabitants.jpg',
-			layout: 'portrait'
+			name: 'Gatsby',
+			thumb: 'images/thumbs/gatsby.jpg',
+			full: 'images/full/gatsby.jpg',
+			thumbStyle: 'height2'
 		},
 		{
 			name: 'Wrought',
-			thumb: 'images/thumbs/wrought_thumb.jpg',
+			thumb: 'images/thumbs/wrought.jpg',
 			full: 'images/full/wrought.jpg',
-			layout: 'landscape'
+			thumbStyle: ''
+		},
+		{
+			name: 'Inhabitants',
+			thumb: 'images/thumbs/inhabitants.jpg',
+			full: 'images/full/inhabitants.jpg',
+			thumbStyle: ''
+		},
+
+		{
+			name: 'Lysol',
+			thumb: 'images/thumbs/lysol.jpg',
+			full: 'images/full/lysol.jpg',
+			thumbStyle: 'width2'
 		},
 		{
 			name: 'Realm',
-			thumb: 'images/thumbs/realm_thumb.jpg',
+			thumb: 'images/thumbs/realm.jpg',
 			full: 'images/full/realm.jpg',
-			layout: 'portrait'
+			thumbStyle: ''
 		}
 	];
 })();
